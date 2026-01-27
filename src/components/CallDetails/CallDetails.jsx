eedca58ead57a434f20509e3f1bcd853a4a3a84b
antnose <antnose28@email.com>
2026-01-28 00:34:57 +0600

    Add call Details

---
+++ src/components/CallDetails/CallDetails.jsx
@@ -0,0 +1,55 @@
+import playBtn from "../../assets/playButton.png";
+
+const CallDetails = () => {
+  return (
+    <div className="col-span-7 bg-[#111827] rounded-2xl border border-slate-800">
+      <h2 className="p-5 text-white text-lg font-semibold border-b border-slate-800">
+        Call List
+      </h2>
+
+      <div className="p-5">
+        <div className="grid grid-cols-2 gap-y-6 mb-8 ">
+          <div>
+            <p className="text-slate-500 text-xs mb-1">Phone Number</p>
+            <p className="text-white font-medium">+1 (555) 123-4567</p>
+          </div>
+
+          <div>
+            <p className="text-slate-500 text-xs mb-1">Duration</p>
+            <p className="text-white font-medium">4:32</p>
+          </div>
+
+          <div>
+            <p className="text-slate-500 text-xs mb-1">Date & Time</p>
+            <p className="text-white font-medium">2025-12-16 10:45 AM</p>
+          </div>
+
+          <div>
+            <p className="text-slate-500 text-xs mb-1">Issue Type</p>
+            <p className="text-white font-medium">Screen</p>
+          </div>
+
+          <div>
+            <p className="text-slate-500 text-xs mb-1">Call Type</p>
+            <span className="text-emerald-400 bg-emerald-400/10 text-[11px] px-3 py-1 rounded-lg">
+              AI Resolved
+            </span>
+          </div>
+
+          <br />
+          <div className="w-full">
+            <p className="text-slate-500 text-xs mb-1">Outcome</p>
+            <p className="text-white font-medium">Quote provided</p>
+          </div>
+        </div>
+
+        <button className="w-full flex items-center justify-center gap-2 py-3 px-6 rounded-full border-t border-[#AD46FF]/30 bg-linear-to-r from-[#2e1a4a] via-[#3b1c5a] to-[#2e1a4a] text-[#C084FC] hover:brightness-110 transition-all shadow-inner">
+          <img src={playBtn} alt="" />
+          <span className="font-medium text-lg">Play Audio Recording</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CallDetails;
--- src/components/CallLogs/CallLogs.jsx
+++ src/components/CallLogs/CallLogs.jsx
@@ -1,3 +1,4 @@
+import CallDetails from "../CallDetails/CallDetails";
 import CallList from "../CallList/CallList";
 
 const CallLogs = () => {
@@ -5,6 +6,9 @@ const CallLogs = () => {
     <div className="grid grid-cols-12 gap-6 p-6 min-h-screen bg-dashboard">
       {/* Call List */}
       <CallList />
+
+      {/* Call Details */}
+      <CallDetails />
     </div>
   );
 };
