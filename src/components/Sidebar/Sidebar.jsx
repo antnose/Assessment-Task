c842b3c7d018ce5d97259b98ebf0658ad7dc3b2e
antnose <antnose28@email.com>
2026-01-27 22:14:00 +0600

    Show sidebar icon

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,15 +1,44 @@
+import { Link } from "react-router";
+import dashboardIcon from "../../assets/sidebar/home.png";
+import callIcon from "../../assets/sidebar/call.png";
+import appointmentIcon from "../../assets/sidebar/Appointments.png";
+import settingsIcon from "../../assets/sidebar/Settings.png";
+
 const Sidebar = () => {
   const options = [
-    "Dashboard Overview",
-    "Call Logs",
-    "Appointments",
-    "Settings",
+    {
+      name: "Dashboard Overview",
+      path: "/",
+      icon: dashboardIcon,
+    },
+    {
+      name: "Call Logs",
+      path: "/calls",
+      icon: callIcon,
+    },
+    {
+      name: "Appointments",
+      path: "/appointments",
+      icon: appointmentIcon,
+    },
+    {
+      name: "Settings",
+      path: "/settings",
+      icon: settingsIcon,
+    },
   ];
 
   return (
     <div>
       {options.map((option) => (
-        <div className="px-3 font-semibold text-xl py-3 ">{option}</div>
+        <Link
+          key={option.path}
+          to={option.path}
+          className="flex items-center gap-3 px-3 font-semibold text-xl py-3"
+        >
+          <img src={option.icon} alt="" />
+          {option.name}
+        </Link>
       ))}
     </div>
   );
